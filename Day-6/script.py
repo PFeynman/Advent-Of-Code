181cbf202dd8f8cf38b209d29bf9e15c35618fd0
Pablo Rey Pedrosa <pablo.rey@pccomponentes.com>
2022-12-07 22:34:27 +0100

    Day 6 solution

--- Day-6/script.py
+++ Day-6/script.py
@@ -1,8 +1,21 @@
 def part_1(message):
-    return None
+    chunk_size = 14
+    begin = 0
+
+    while begin + chunk_size < len(message):
+        prev_begin = begin
+        chunk = message[begin:begin + chunk_size]
+        for i in range(chunk_size):
+            if (f := chunk.find(chunk[i], i + 1)) != -1:
+                begin += (i + 1)
+                break
+        if prev_begin == begin:
+            break
+    
+    return begin + chunk_size
+
 if __name__ == "__main__":
     with open('input.txt', 'r') as input:
-        messages = [x for x in input.readlines()]
-
-    for message in messages:
-        print(part_1(message))
+        message = input.read()
+    
+    print(part_1(message))

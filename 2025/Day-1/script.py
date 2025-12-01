8909a8b0d71177a9dcd342eaf87e7e091b9adbb4
Pablo Rey <pablo.reypedrosa@gmail.com>
2025-12-01 15:20:26 +0000

    Day 1 solution 2

--- 2025/Day-1/script.py
+++ 2025/Day-1/script.py
@@ -21,19 +21,34 @@ def part_2(lines):
 
   for line in lines:
     turn_direction = line[0]
-    turn_positions = int(line[1:])
+    raw_turn_positions = int(line[1:])
+    complete_turns = int(raw_turn_positions / 100)
+    turn_positions = raw_turn_positions % 100
+    passed_through_0 = False
+
     if turn_direction == "L":
-      dial_position = (dial_position - turn_positions) % 100
+      dial_position = dial_position - turn_positions
+      if dial_position < 0 and dial_position + turn_positions > 0:
+        passed_through_0 = True
+      dial_position = dial_position % 100
     else:
-      dial_position = (dial_position + turn_positions) % 100
+      dial_position = dial_position + turn_positions
+      if dial_position > 100:
+        passed_through_0 = True
+      dial_position = dial_position % 100
     
+    times_dial_0 += complete_turns
+
+    if passed_through_0:
+      times_dial_0 += 1
+
     if dial_position == 0:
       times_dial_0 += 1
     
   return times_dial_0
   
 if __name__ == "__main__":
-  with open('test_input.txt', 'r') as input:
+  with open('input.txt', 'r') as input:
     lines = [i.rstrip() for i in input.readlines()]
 
-  print(part_1(lines))
+  print(part_2(lines))

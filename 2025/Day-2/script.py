b840536623ac054cb58fdf57d57035195f6fcf85
Pablo Rey <pablo.reypedrosa@gmail.com>
2025-12-02 09:58:55 +0000

    Day 2 solution 1

--- 2025/Day-2/script.py
+++ 2025/Day-2/script.py
@@ -1,5 +1,17 @@
+import re
+
+def part_1(ranges):
+  sum = 0
+  for r in ranges:
+    ids = r.split('-')
+    for i in range(int(ids[0]), int(ids[1]) + 1):
+      result = re.search(r"(^\d+)\1$", str(i))
+      if result != None:
+        sum += int(result.group())
+  return sum
+
 if __name__ == "__main__":
   with open('input.txt', 'r') as input:
-    lines = [i.rstrip() for i in input.readlines()]
+    ranges = [r for line in input for r in line.rstrip().split(',')]
 
-  print(part_2(lines))
+  print(part_1(ranges))

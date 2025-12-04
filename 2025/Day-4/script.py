a44e963afe941ab1ddbb3d09f005a6c067cab0ee
Pablo Rey <pablo.reypedrosa@gmail.com>
2025-12-04 21:51:27 +0000

    Day 4 solution 2

--- 2025/Day-4/script.py
+++ 2025/Day-4/script.py
@@ -36,8 +36,50 @@ def part_1(grid):
             
   return sum
 
+def part_2(banks):
+  sum = 0
+  height = len(grid)
+  width  = len(grid[0])
+  
+  while True:
+    removed = []
+    for i in range(height):
+      for j in range(width):
+        accessible = True
+        adjacent_rolls = 0
+        if grid[i][j] == '@':
+          for i_offset in range(-1, 2):
+            for j_offset in range(-1, 2):
+              if is_out_of_bound(i + i_offset, j + j_offset, height, width):
+                continue
+
+              if i_offset == 0 and j_offset == 0:
+                continue
+              
+              if grid[i + i_offset][j + j_offset] == '@':
+                adjacent_rolls += 1
+              
+              if adjacent_rolls > 3:
+                accessible = False
+                break
+            
+            if not accessible:
+              break
+          
+          if accessible:
+            sum += 1
+            removed.append([i, j])
+
+    if len(removed) == 0:
+      break
+    
+    for element in removed:
+      grid[element[0]][element[1]] = '.'
+ 
+  return sum
+
 if __name__ == "__main__":
   with open('input.txt', 'r') as input:
     grid = [list(i.rstrip()) for i in input.readlines()]
 
-  print(part_1(grid))
+  print(part_2(grid))

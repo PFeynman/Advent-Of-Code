f17d2ff39884a11667fdd4bd491dfc4a781624bf
Pablo Rey <pablo.reypedrosa@gmail.com>
2025-12-05 15:49:41 +0000

    Day 5 solution 2 - first approach

--- 2025/Day-5/script.py
+++ 2025/Day-5/script.py
@@ -12,6 +12,15 @@ def part_1(fresh_ids_ranges, available_ids):
 
   return sum
 
+def part_2(fresh_ids_ranges):
+  union = set()
+
+  for id_range in fresh_ids_ranges:
+    tmp = set(range(id_range[0], id_range[1] + 1))
+    union = union | tmp
+  
+  return len(union)
+
 if __name__ == "__main__":
   with open('input.txt', 'r') as input:
     fresh_ids_ranges, available_ids = (i.splitlines() for i in input.read().strip('\n').split('\n\n'))
@@ -20,3 +29,4 @@ if __name__ == "__main__":
   available_ids = list(map(int, available_ids))
   
   print(f'Part 1 solution: {part_1(fresh_ids_ranges, available_ids)}')
+  print(f'Part 2 solution: {part_2(fresh_ids_ranges)}')

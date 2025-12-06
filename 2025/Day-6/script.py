6f339282e1f14c5638a3c3f5e51ca7347e98fd43
Pablo <pablo.reypedrosa@gmail.com>
2025-12-06 21:45:25 +0100

    Day 6 solution 2

--- 2025/Day-6/script.py
+++ 2025/Day-6/script.py
@@ -1,4 +1,5 @@
 import math
+import pprint
 
 def part_1(operations):
   total = 0
@@ -16,8 +17,37 @@ def part_1(operations):
 
   return total
 
-if __name__ == "__main__":
-  with open('test_input.txt', 'r') as input:
-    operations = [i.rstrip().split() for i in input.readlines()]
+def part_2(operations):
+  total = 0
+  operands = []
+  lines = len(operations)
+  delimiter = ' ' * (lines - 1)
 
-  print(f'Part 1 solution: {part_1(operations)}')
+  for j in range(len(operations[0]) - 1, -1, -1):
+    operand = ''
+    for i in range(lines):
+      if i == lines - 1:
+        if operand == delimiter:
+          continue
+        else:
+          operands.append(int(operand))
+          operand = ''
+          if operations[i][j] != ' ':
+            if operations[i][j] == '+':
+              total += sum(operands)
+            else:
+              total += math.prod(operands)
+            operands = []
+      else:
+        operand += operations[i][j]
+
+  return total
+
+if __name__ == "__main__":
+  with open('input.txt', 'r') as input:
+    operations = [i.rstrip('\n') for i in input.readlines()]
+
+    operations_part1 = [i.rstrip().split() for i in operations]
+
+  print(f'Part 1 solution: {part_1(operations_part1)}')
+  print(f'Part 2 solution: {part_2(operations)}')

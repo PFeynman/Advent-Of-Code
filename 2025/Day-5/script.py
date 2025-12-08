222e7b21f4b0707dd54a946e6d1d203efc6ab95d
Pablo <pablo.reypedrosa@gmail.com>
2025-12-08 21:24:40 +0100

    Day 5 solution 2 iteration

--- 2025/Day-5/script.py
+++ 2025/Day-5/script.py
@@ -16,28 +16,33 @@ def part_2(fresh_ids_ranges):
   
   count = 0
 
+  fresh_ids_ranges.sort()
+
   selected_ranges = [fresh_ids_ranges[0]]
 
   for id_range in fresh_ids_ranges[1:]:
     print(f'Checking: {id_range}')
     selectable = True
     for selected in enumerate(selected_ranges):
-      print(f'Selected: {selected}')
       if id_range[0] < selected[1][0] and id_range[1] > selected[1][1]:
+        print("Case 1")
         # The checking range is a superset of the already selected one.
         selected_ranges[selected[0]] = id_range
         selectable = False
         break
       elif id_range[0] < selected[1][0] and id_range[1] >= selected[1][0] and id_range[1] < selected[1][1]:
+        print("Case 2")
         # The already selected and the checking range intersect. The lower bound is of the checking one
         selected_ranges[selected[0]][0] = id_range[0]
         selectable = False
         break
       elif id_range[0] > selected[1][0] and id_range[1] < selected[1][1]:
         # The checking range is inside the already selected one
+        print("Case 3")
         selectable = False
         break
       elif id_range[0] > selected[1][0] and id_range[0] < selected[1][1] and id_range[1] >= selected[1][1]:
+        print("Case 4")
         # The already selected and the checking range intersect. The upper bound is of the checking one
         selected_ranges[selected[0]][1] = id_range[1]
         selectable = False
@@ -45,9 +50,12 @@ def part_2(fresh_ids_ranges):
     
     if selectable:
       selected_ranges.append(id_range)
+    print(f'Selected ranges: {selected_ranges}')
 
-  print(len(selected_ranges))
-  return None
+  for selected_range in selected_ranges:
+    count += selected_range[1] - selected_range[0] + 1
+    
+  return count
 
 if __name__ == "__main__":
   with open('input.txt', 'r') as input:
@@ -55,7 +63,6 @@ if __name__ == "__main__":
   
   fresh_ids_ranges = [list(map(int, r.split("-"))) for r in fresh_ids_ranges]
   available_ids = list(map(int, available_ids))
-
 
   print(f'Part 1 solution: {part_1(fresh_ids_ranges, available_ids)}')
   print(f'Part 2 solution: {part_2(fresh_ids_ranges)}')

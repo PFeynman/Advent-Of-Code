c0417e31923c95d379bc75d980aa327356fbe5be
Pablo <pablo.reypedrosa@gmail.com>
2025-12-09 22:08:18 +0100

    Day 7 solution 2 - first approach recursive

--- 2025/Day-7/script.py
+++ 2025/Day-7/script.py
@@ -17,36 +17,26 @@ def part_1():
 
   return splits
 
-def part_2():
-  timelines = 0
-  s = (0, tachyon_manifold[0].index('S'))
-  tachyon_manifold[1][s[1]] = '|'
-
-  for i in range(2, len(tachyon_manifold)):
-    for j in range(len(tachyon_manifold[i])):
-      if tachyon_manifold[i][j] == '^' and tachyon_manifold[i-1][j] == '|':
-        tachyon_manifold[i][j-1] = tachyon_manifold[i][j+1] = '|'
-      if tachyon_manifold[i][j] == '.' and tachyon_manifold[i-1][j] == '|':
-        tachyon_manifold[i][j] = '|'
-  
-  pprint.pp(tachyon_manifold)
-
-  stack = []
-  stack.append(s)
-  stack.append((current[0] + 1, current[1]))
-
-  while len(stack) > 0:
-    current = stack.pop()
-    if tachyon_manifold[current[0]][current[1]] == 'S':
-      stack.append((current[0] + 1, current[1]))
+def timelines(point):
+  current_point = point
+  for i in range(point[0], len(tachyon_manifold)):
     
-    print(stack)
-    return None
+  if point[0] == len(tachyon_manifold) - 1:
+    return 1
+  elif point[1] < 0 or point[1] >= len(tachyon_manifold[0]):
+    return 0
+  elif tachyon_manifold[point[0]][point[1]] == '^':
+    return timelines((point[0] + 1, point[1] - 1)) + timelines((point[0] + 1, point[1] + 1))
+  else:
+    return timelines((point[0] + 1, point[1]))
+
+def part_2():
+  s = (0, tachyon_manifold[0].index('S'))
   
-  return None
+  return timelines(s)
 
 if __name__ == "__main__":
-  with open('test_input.txt', 'r') as input:
+  with open('input.txt', 'r') as input:
     tachyon_manifold = [list(i.rstrip('\n')) for i in input.readlines()]
 
   print(f'Part 1 solution: {part_1()}')

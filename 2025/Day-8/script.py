84d1eef824cdd63d513f269c9d93fc2878c8ff23
Pablo <pablo.reypedrosa@gmail.com>
2025-12-09 18:16:24 +0100

    Day 8 solution 2

--- 2025/Day-8/script.py
+++ 2025/Day-8/script.py
@@ -39,6 +39,13 @@ class UnionFind:
     print("Conjuntos actuales:")
     for root, elements in sets.items():
         print(f"  {root} -> {elements}")
+  
+  def isConnected(self):
+    root = self.parent[0]
+    for i in range(1, len(self.parent)):
+        if self.find(i) != root:
+            return False
+    return True
 
 def part_1(junction_boxes):
   distances = np.zeros((len(junction_boxes), len(junction_boxes)), dtype=float)
@@ -65,8 +72,26 @@ def part_1(junction_boxes):
   return math.prod(sizes[0:3])
 
 def part_2(junction_boxes):
-  
-  return None
+  distances = np.zeros((len(junction_boxes), len(junction_boxes)), dtype=float)
+
+  for i in range(len(junction_boxes)):
+    for j in range(len(junction_boxes)):
+      distances[i][j] = math.dist(junction_boxes[i], junction_boxes[j])
+
+  circuits = UnionFind(len(junction_boxes))
+
+  min_val = 0
+  connections_done = 0
+  current = 0
+  while circuits.isConnected() == False:
+    masked = np.where(distances > min_val, distances, np.inf)
+    flat_idx = np.argmin(masked)
+    idx = np.unravel_index(flat_idx, distances.shape)
+    min_val = distances[idx]
+    circuits.union(idx[0].item(), idx[1].item())
+    connections_done += 1
+
+  return junction_boxes[idx[0].item()][0] * junction_boxes[idx[1].item()][0]
 
 if __name__ == "__main__":
   with open('input.txt', 'r') as input:

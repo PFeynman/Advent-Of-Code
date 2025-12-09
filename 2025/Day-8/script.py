a7bd943b434233d25a294f6bbdb06f92df3458ed
Pablo <pablo.reypedrosa@gmail.com>
2025-12-09 17:26:36 +0100

    Day 8 solution 1

--- 2025/Day-8/script.py
+++ 2025/Day-8/script.py
@@ -24,6 +24,9 @@ class UnionFind:
             self.parent[rootY] = rootX
             self.size[rootX] += self.size[rootY]
   
+  def get_sizes(self):
+    return self.size
+  
   def print_sets(self):
     sets = {}
     print(self.parent)
@@ -45,38 +48,28 @@ def part_1(junction_boxes):
       distances[i][j] = math.dist(junction_boxes[i], junction_boxes[j])
 
   circuits = UnionFind(len(junction_boxes))
-  circuits.print_sets()
 
-  # 0: 162,817,812 and 425,690,689
-  # 1: 162,817,812 and 431,825,988
-  # 2: 906,360,560 and 805,96,715
-  # 3: 431,825,988 and 425,690,689 -> Nothing happens
   min_val = 0
   connections_done = 0
-  while connections_done < 10:
+  while connections_done < 1000:
     masked = np.where(distances > min_val, distances, np.inf)
     flat_idx = np.argmin(masked)
     idx = np.unravel_index(flat_idx, distances.shape)
     min_val = distances[idx]
-    print(idx)
-    print(junction_boxes[idx[0]], junction_boxes[idx[1]])
     circuits.union(idx[0].item(), idx[1].item())
     connections_done += 1
-    #if circuits[idx[1]]: 
-    #  circuits[idx[0]].append(idx[1].item())
-    #  connections_done += 1    
-    #distances[idx[1], :] = 0
-    #distances[:, idx[1]] = 0
 
-  circuits.print_sets()
-  return None
+  sizes = circuits.get_sizes()
+  sizes.sort(reverse=True)
+
+  return math.prod(sizes[0:3])
 
 def part_2(junction_boxes):
   
   return None
 
 if __name__ == "__main__":
-  with open('test_input.txt', 'r') as input:
+  with open('input.txt', 'r') as input:
     junction_boxes = [list(map(int, i.rstrip().split(','))) for i in input.readlines()]
 
   print(f'Part 1 solution: {part_1(junction_boxes)}')

1f81cbb98d6a7732a6bc87c622df7cf25c3b5630
Pablo Rey <pablo.reypedrosa@gmail.com>
2025-12-09 16:06:49 +0000

    Day 8 solution 1 - implement DSU

--- 2025/Day-8/script.py
+++ 2025/Day-8/script.py
@@ -3,6 +3,40 @@ import math
 import numpy as np
 import time
 
+class UnionFind:
+  def __init__(self, size):
+      self.parent = [i for i in range(size)]
+      self.size = [1] * size
+  
+  def find(self, x):
+      while x != self.parent[x]:
+          x = self.parent[x]
+      return x
+  
+  def union(self, x, y):
+      rootX = self.find(x)
+      rootY = self.find(y)
+      if rootX != rootY:
+        if self.size[rootX] < self.size[rootY]:
+            self.parent[rootX] = rootY
+            self.size[rootY] += self.size[rootX]
+        else:
+            self.parent[rootY] = rootX
+            self.size[rootX] += self.size[rootY]
+  
+  def print_sets(self):
+    sets = {}
+    print(self.parent)
+    for i in range(len(self.parent)):
+        root = self.find(i)
+        if root not in sets:
+            sets[root] = []
+        sets[root].append(i)
+
+    print("Conjuntos actuales:")
+    for root, elements in sets.items():
+        print(f"  {root} -> {elements}")
+
 def part_1(junction_boxes):
   distances = np.zeros((len(junction_boxes), len(junction_boxes)), dtype=float)
 
@@ -10,13 +44,14 @@ def part_1(junction_boxes):
     for j in range(len(junction_boxes)):
       distances[i][j] = math.dist(junction_boxes[i], junction_boxes[j])
 
-  circuits = [[i] for i in range(len(junction_boxes))]
+  circuits = UnionFind(len(junction_boxes))
+  circuits.print_sets()
+
   # 0: 162,817,812 and 425,690,689
   # 1: 162,817,812 and 431,825,988
   # 2: 906,360,560 and 805,96,715
   # 3: 431,825,988 and 425,690,689 -> Nothing happens
   min_val = 0
-  iter_count = 0
   connections_done = 0
   while connections_done < 10:
     masked = np.where(distances > min_val, distances, np.inf)
@@ -25,16 +60,15 @@ def part_1(junction_boxes):
     min_val = distances[idx]
     print(idx)
     print(junction_boxes[idx[0]], junction_boxes[idx[1]])
-    
-    if circuits[idx[1]]: 
-      circuits[idx[0]].append(idx[1].item())
-      connections_done += 1
-    circuits[idx[1]].clear()
-    print(circuits)
-    distances[idx[1], :] = 0
-    distances[:, idx[1]] = 0
+    circuits.union(idx[0].item(), idx[1].item())
+    connections_done += 1
+    #if circuits[idx[1]]: 
+    #  circuits[idx[0]].append(idx[1].item())
+    #  connections_done += 1    
+    #distances[idx[1], :] = 0
+    #distances[:, idx[1]] = 0
 
-  print(circuits)
+  circuits.print_sets()
   return None
 
 def part_2(junction_boxes):
